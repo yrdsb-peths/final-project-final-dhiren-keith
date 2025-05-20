1f1989fd4fcf71d89b9fcf61879ea600bcfbcf1a
DhirenSutha <348599382@gapps.yrdsb.ca>
2025-05-20 09:02:21 -0400

    Corrected Names

--- Goon_Three.java
+++ Goon_Three.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Goon_3 here.
+ * Write a description of class Goon_Three here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Goon_3 extends Actor
+public class Goon_Three extends Actor
 {
     /**
-     * Act - do whatever the Goon_3 wants to do. This method is called whenever
+     * Act - do whatever the Goon_Three wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
--- Goon_Two.java
+++ Goon_Two.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Goon_2 here.
+ * Write a description of class Goon_Two here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Goon_2 extends Actor
+public class Goon_Two extends Actor
 {
     /**
-     * Act - do whatever the Goon_2 wants to do. This method is called whenever
+     * Act - do whatever the Goon_Two wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
