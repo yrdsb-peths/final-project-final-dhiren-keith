ab5cc8acf122c3167b13b655cc0c44de9555bb48
DhirenSutha <348599382@gapps.yrdsb.ca>
2025-05-23 08:58:55 -0400

    Added Image Worlds

---
+++ SelectWorldTwo.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SelectWorldTwo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SelectWorldTwo extends Actor
+{
+    /**
+     * Act - do whatever the SelectWorldTwo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
