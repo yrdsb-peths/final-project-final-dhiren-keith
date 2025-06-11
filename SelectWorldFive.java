28e2df5159293e66449c9fe655231598c7ffab4e
DhirenSutha <348599382@gapps.yrdsb.ca>
2025-06-10 23:00:36 -0400

    Comments on world

--- SelectWorldFive.java
+++ SelectWorldFive.java
@@ -10,25 +10,31 @@ import greenfoot.*;
  */
 public class SelectWorldFive extends Actor {
     public int persistentLevel;
-
+    
+    /**
+     *  Constructor sets and scales the image for the world selector
+     */
     public SelectWorldFive() {
         GreenfootImage image = new GreenfootImage("5.png");
         image.scale(75, 75);
         setImage(image);
     }
 
+    /**
+     *  Detects clicks on selector, if level is 80 world five is unlocked
+     */
     public void act() {
         if (Greenfoot.mouseClicked(this)) {
             int currentLevel = Hero.getPersistentLevel();
             if (currentLevel == 80) {
                 WorldFive worldFive = new WorldFive();
-                Greenfoot.setWorld(worldFive);
+                Greenfoot.setWorld(worldFive); // Load level five
             } else {
                 int x = getWorld().getWidth() / 2;
                 int y = getWorld().getHeight() / 2;
                 getWorld().showText("World Five is only for level 80! Current level: " + currentLevel, x, y);
                 Greenfoot.delay(120);
-                getWorld().showText("", x, y);
+                getWorld().showText("", x, y); // Clear the message
             }
         }
     }
